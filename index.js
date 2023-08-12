5233139fff8cb858f9e3f30e2c68c10455969734
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-08-13 01:03:32 +0500

    completed the chalange for the certification

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage }).single('upfile');
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +15,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload, (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 
 
